689651938358220f2ff856d6779f9aa9191e93c7
symbiontik <5622331-symbiontik@users.noreply.gitlab.com>
2022-12-17 12:11:27 -0600

    update README with diagram and overview

--- src/components/yelpApiRequest.tsx
+++ src/components/yelpApiRequest.tsx
@@ -4,8 +4,10 @@ import axios from 'axios';
 const YelpAPIRequest = () => {
 const config = {
     headers: {
-      "Access-Control-Allow-Origin" : "*",
-      "Authorization" : "Bearer 9nntrke6fVf6C4oEJzpaF2W419UbQR2CojDf_KDnRfKvjIAqwm3aKvifqhVciLrNP72xUiZDh52CVetmBXqbAt_7zO0PiD8PF00RCKRQT5gNaNU0mqnu3FNjZZucY3Yx"
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      Authorization:
+        "Bearer 9nntrke6fVf6C4oEJzpaF2W419UbQR2CojDf_KDnRfKvjIAqwm3aKvifqhVciLrNP72xUiZDh52CVetmBXqbAt_7zO0PiD8PF00RCKRQT5gNaNU0mqnu3FNjZZucY3Yx",
     },
     params: {
       term: "restaurants",
