c7d7d34c1826707f6c92427ee120b7995e6ade73
tatedean <dean.e.tate@gmail.com>
2022-12-28 10:40:41 -0600

    added filter framework, added images and pricing

--- src/components/randomRest.tsx
+++ src/components/randomRest.tsx
@@ -2,18 +2,37 @@ import React, { Component, useState } from 'react';
 
 export default function RandomRest({restData}) {
 
-  const [R, setR] = useState("");
+  const [R, setR] = useState({'name': '', 'price': '', 'image_url' : ''});
   
   function getRandomRest() {
       const random = Math.floor(Math.random() * restData.length);
-      setR(restData[random].name);
+      setR(restData[random]);
+  }
+
+  function getPrices() {
+    const noPricing = restData.filter(rest => !rest.hasOwnProperty('price')).length
+    return(
+    <>
+      <label htmlFor="filterPrice">Pricing: </label>
+      <select id="filterPrice" multiple>
+        <option value="$" selected>$: {restData.filter(rest => rest.price === '$').length + noPricing}</option>
+        <option value="$$" selected>$$: {restData.filter(rest => rest.price === '$$').length}</option>
+        <option value="$$$" selected>$$$: {restData.filter(rest => rest.price === '$$$').length}</option>
+        <option value="$$$$" selected>$$$$: {restData.filter(rest => rest.price === '$$$$').length}</option>
+      </select>
+    </>
+    )
   }
   
   return(
       <div>
+        {getPrices()}
+        <br></br>
         <button className="randomRest-btn" onClick={getRandomRest}>Pick Random Restaurant</button>
         <p>You should go to:</p>
-        <h2>{R}</h2>
+        <h2>{R.name}</h2>
+        <p>{R.price ? R.price : '$'}</p>
+        <img src={R.image_url}></img>
       </div>
     )
 }
